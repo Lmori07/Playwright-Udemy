c8e01156f28e8795eca6ac9c5588bea6eadddafb
Lmori07 <33818263+Lmori07@users.noreply.github.com>
2022-08-14 14:29:32 -0400

    Wait setup process

--- tests/UIBasics.spec.js
+++ tests/UIBasics.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require("@playwright/test");
 
-test("Browser PW testing code", async ({ browser }) => {
-  /*In Javascript code is async that mean is not run in squence, using command await it will
+test.only("Browser PW testing code", async ({ browser }) => {
+  /*In Javascript code is async that mean is not run in squence, using command await it will tell
     JS that it needs to wait for that step to be completed. Also the '=>' represent anonymous function*/
 
   /*{browser} is curly brack represent an element of playwright and not a simple browser object for JS*/
@@ -10,6 +10,30 @@ test("Browser PW testing code", async ({ browser }) => {
   const page = await initialBrowser.newPage();
   await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
   console.log(await page.title());
+  await page.locator("#username").type("rahulshettyacademy");
+  await page.locator("[name=password]").type("pass");
+  await page.locator("#signInBtn").click();
+  console.log(await page.locator("[style*=block]").textContent());
+  //Assertion
+  await expect(page.locator("[style*=block]")).toContainText("Incorrect");
+
+  await page.locator("[name=password]").fill("learning");
+  /*we also need to identify if our application is client-side or not so we can used a wait
+  mechanism that allow us to interact with the elements that we need*/
+
+  /*This promise reeference is in place to let us know he will wait for Navigation even rigth
+  after the click even because they are wrap up in a promise statement that will be executed
+  and return something.*/
+  await Promise.all([
+    page.waitForNavigation(),
+    page.locator("#signInBtn").click(),
+  ]);
+  //console.log(await page.locator(".card-body a").first().textContent());
+  /*if we remove the line above this will fail, becuase playwright have not implemented yet
+  a wait mechanism for allTextContest and will pring a bla array is still valid but will not
+  print all header as we expected, but because the previous line is in effect it will wait until
+  page load, so all element are loaded and allTextContest then can grab all the text and print it.*/
+  console.log(await page.locator(".card-body a").allTextContents());
 });
 
 test("Page PW testing code", async ({ page }) => {
